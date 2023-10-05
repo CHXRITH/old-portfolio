5cc5be63829be62bc8cf3d42dfd09dc565038cef
CHXRITH <141550562+CHXRITH@users.noreply.github.com>
2023-10-05 06:36:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Add this to your script.js
 window.addEventListener('load', () => {
-    // Remove the loader once the page is fully loaded
-    const loader = document.querySelector('.loader');
-    loader.style.display = 'none';
+    // Remove the preloader once the page is fully loaded
+    const preloader = document.querySelector('.preloader');
+    preloader.style.display = 'none';
 });
